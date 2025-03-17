71fd291aae6f96e47caaf0465ff2582f20ec9e1b
heeun98 <132757475+heeun98@users.noreply.github.com>
2025-03-17 18:52:51 +0900

    11111추가

--- src/git/Student.java
+++ src/git/Student.java
@@ -5,5 +5,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("수정사항");
         System.out.println(1);
+
+        System.out.println("1111111");
     }
 }
